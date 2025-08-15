89e77fae33511b1468fcb20549ebb4e2bc6ddbb2
ismat-samadov <ismetsemedov@gmail.com>
2025-08-15 23:15:23 +0400

    init

--- src/components/HeicConverter.tsx
+++ src/components/HeicConverter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Image from 'next/image';
 
